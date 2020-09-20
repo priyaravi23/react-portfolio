3d78e64f564c6c2b012f3127c6ff992b3b3fc6ab
Priya Ravi <priyaravi23@gmail.com>
2020-09-20 10:38:27 -0700

    Updates

---
+++ precache-manifest.09adf631da2eba450e397980e4ebdc87.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c00b44851d9f538aaf9fbe07366a2f6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f2b35a1c3ce95c4e30ce",
+    "url": "/react-portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "4f41d88acfaeb0e4e87d",
+    "url": "/react-portfolio/static/css/main.991a6ac8.chunk.css"
+  },
+  {
+    "revision": "f2b35a1c3ce95c4e30ce",
+    "url": "/react-portfolio/static/js/2.dfef767e.chunk.js"
+  },
+  {
+    "revision": "bdcfdbef37b771299e3e4252a0d10375",
+    "url": "/react-portfolio/static/js/2.dfef767e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f41d88acfaeb0e4e87d",
+    "url": "/react-portfolio/static/js/main.e9801c4d.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "73bde4490e5db0a23f1cbccc30a607a1",
+    "url": "/react-portfolio/static/media/abstract.73bde449.jpg"
+  },
+  {
+    "revision": "b76a5c8a07af74ab80c34fd8ae720c03",
+    "url": "/react-portfolio/static/media/avatar.b76a5c8a.png"
+  },
+  {
+    "revision": "1dcf6b68cc8f07eef99834c877f9abd0",
+    "url": "/react-portfolio/static/media/berkeley.1dcf6b68.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/react-portfolio/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "e25a9298bc6897adebd33203fd02af9f",
+    "url": "/react-portfolio/static/media/carleton.e25a9298.png"
+  },
+  {
+    "revision": "b50b8274732a363a14f7f762e0a6d64d",
+    "url": "/react-portfolio/static/media/infinity.b50b8274.png"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/react-portfolio/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/react-portfolio/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/react-portfolio/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "d262c9ceae6ae9b53a2c9f477ceac5f7",
+    "url": "/react-portfolio/static/media/pittsburgh.d262c9ce.png"
+  },
+  {
+    "revision": "19080b3cc1962fff3d289ffd360e640d",
+    "url": "/react-portfolio/static/media/redux-logo.19080b3c.png"
+  },
+  {
+    "revision": "d03c06001a3a3a81ab720b65b2b6972f",
+    "url": "/react-portfolio/static/media/run-buddy.d03c0600.png"
+  },
+  {
+    "revision": "2c035b0740a1ebf4ffc7f0e536d209da",
+    "url": "/react-portfolio/static/media/specialty-market.2c035b07.png"
+  },
+  {
+    "revision": "a5f4c5aca864a44efefc47868ee2aa09",
+    "url": "/react-portfolio/static/media/task-tracker.a5f4c5ac.png"
+  },
+  {
+    "revision": "85ac172bceb3ae4080b76d44d1f985b3",
+    "url": "/react-portfolio/static/media/voltron.85ac172b.png"
+  },
+  {
+    "revision": "ba4481185ccf437497c9ff2e59a80972",
+    "url": "/react-portfolio/static/media/weather-scanner.ba448118.png"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/react-portfolio/static/media/webpack-logo.f7099627.png"
+  }
+]);
