562ea9ee270d6d3d3ccf1e87340bc350f4b6b37b
Priya Ravi <priyaravi23@gmail.com>
2020-09-20 14:52:30 -0700

    added links to github and deployment site to apps in portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -26,102 +26,120 @@ const Portfolio = () => {
 
                                 <div className="grid-container vertical-line">
                                     <div className="grid-item gallery__item--1">
-                                        <a href='https://react-todo-cypress.herokuapp.com/' target="_blank">
-                                            <img src={Todo} alt="Task Tracker App"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Task Tracker</h5>
-                                                <p>React/Cypress</p>
-                                            </div>
-                                        </a>
+                                        <img src={Todo} alt="Task Tracker App"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Task Tracker</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/react-todo-cypress' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://react-todo-cypress.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--2">
-                                        <a href='https://weather-detector-app.herokuapp.com/' target="_blank">
-                                            <img src={WeatherScanner} alt="Weather Scanner App"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Weather Scanner</h5>
-                                                <p>React/Node/REST APIs</p>
-                                            </div>
-                                        </a>
+                                        <img src={WeatherScanner} alt="Weather Scanner App"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Weather Scanner</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/weather-app-client' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://weather-detector-app.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--3">
-                                        <a href='https://infinity-rental.herokuapp.com/' target="_blank">
-                                            <img src={Infinity} alt="Movie Rental App"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Movie Rentals</h5>
-                                                <p>MERN Stack</p>
-                                            </div>
-                                        </a>
+                                        <img src={Infinity} alt="Movie Rental App"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Movie Rentals</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/infinity-rental-client' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://infinity-rental.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--4">
-                                        <a href='https://secure-chamber-23014.herokuapp.com/' target="_blank">
-                                            <img src={WineBlogger} alt="Wine Blogger"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Wine Blogger</h5>
-                                                <p>Handlebars/database/auth/aws-sdk</p>
-                                            </div>
-                                        </a>
+                                        <img src={WineBlogger} alt="Wine Blogger"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Wine Blogger</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/anitapeppercorn/wine-blogger' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://secure-chamber-23014.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--5">
-                                        <a href='https://react-voltron-app.herokuapp.com/' target="_blank">
-                                            <img src={Voltron} alt="Voltron App"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Voltron</h5>
-                                                <p>React/Redux/Cypress/Jest/Local Storage</p>
-                                            </div>
-                                        </a>
+                                        <img src={Voltron} alt="Voltron App"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Voltron</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/react-voltron-app' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://react-voltron-app.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--6">
-                                        <a href='https://specialty-market.herokuapp.com/' target="_blank">
-                                            <img src={SpecialtyMarket} alt="Specialty Market"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Specialty Market</h5>
-                                                <p>React/CSS Grid</p>
-                                            </div>
-                                        </a>
+                                        <img src={SpecialtyMarket} alt="Shop Corner"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Shop Corner</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/groceries-shopping-app' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://specialty-market.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--7">
-                                        <a href='https://priyaravi23.github.io/taskinator/' target="_blank">
-                                            <img src={Taskinator} alt="Taskinator App"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Taskinator</h5>
-                                                <p>Web APIs/Local Storage</p>
-                                            </div>
-                                        </a>
+                                        <img src={Taskinator} alt="Taskinator App"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Taskinator</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/taskinator' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://priyaravi23.github.io/taskinator/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--8">
-                                        <a href='https://pizza-hunt-app.herokuapp.com/' target="_blank">
-                                            <img src={PizzaHunt} alt="Pizza Hunt"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>Pizza Hunt</h5>
-                                                <p>MongDB/IndexedDB/Mongoose</p>
-                                            </div>
-                                        </a>
+                                        <img src={PizzaHunt} alt="Pizza Hunt"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>Pizza Hunt</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/pizza-hunt' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://pizza-hunt-app.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
 
                                     <div className="grid-item gallery__item--9">
-                                        <a href='https://budget-tracker-pwa-app.herokuapp.com/' target="_blank">
-                                            <img src={PwaBudgetTracker} alt="PWA Budget Tracker"
-                                                 className="gallery__img"/>
-                                            <div className="desc">
-                                                <h5>PWA Budget Tracker</h5>
-                                                <p>MongDB/IndexedDB/Mongoose</p>
-                                            </div>
-                                        </a>
+                                        <img src={PwaBudgetTracker} alt="PWA Budget Tracker"
+                                             className="gallery__img"/>
+                                        <div className="desc">
+                                            <h5>PWA Budget Tracker</h5>
+                                        </div>
+
+                                        <div className='dep-links'>
+                                          <a href='https://github.com/priyaravi23/pwa-budget-tracker' target="_blank"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a>
+                                          <a href='https://budget-tracker-pwa-app.herokuapp.com/' target="_blank"><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a>
+                                        </div>
                                     </div>
                                 </div>
 
