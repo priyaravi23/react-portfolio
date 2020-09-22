a203bbd75bf5bef8831b93d3a9de49a3b705ab68
Priya Ravi <priyaravi23@gmail.com>
2020-09-21 20:31:33 -0700

    refactored navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,69 +1,42 @@
 import React from 'react';
-import {
-    HashRouter as Router,
-    Switch,
-    Route,
-    NavLink,
-    Redirect
-} from "react-router-dom";
-import About from '../components/about';
-import Portfolio from '../components/portfolio';
-import Contact from '../components/contact';
-import Resume from '../components/resume';
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
-
     return (
-        <Router>
-            <div className='topnav'>
-                <div className='topnav-left'>
-                    <NavLink
-                        activeClassName="navbar__link--active"
-                        className="navbar__link"
-                        to="/about"
-                    >
-                        ABOUT
-                    </NavLink>
-                </div>
-                <div className='topnav-right'>
-                    <NavLink
-                        activeClassName="navbar__link--active"
-                        className="navbar__link"
-                        to="/portfolio"
-                    >
-                        PORTFOLIO
-                    </NavLink>
-                    <NavLink
-                        activeClassName="navbar__link--active"
-                        className="navbar__link"
-                        to="/contact"
-                    >
-                        CONTACT
-                    </NavLink>
-                    <NavLink
-                        activeClassName="navbar__link--active"
-                        className="navbar__link"
-                        to="/resume"
-                    >
-                        RESUME
-                    </NavLink>
-                </div>
+        <div className='topnav'>
+            <div className='topnav-left'>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/about"
+                >
+                    ABOUT
+                </NavLink>
             </div>
-
-            <Switch>
-                <Route exact path="/">
-                    <Redirect to="/about" />
-                </Route>
-                <Route path="/about">
-                    <About />
-                </Route>
-                <Route path="/portfolio" component={Portfolio} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/resume">
-                    <Resume />
-                </Route>
-            </Switch>
-        </Router>
+            <div className='topnav-right'>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/portfolio"
+                >
+                    PORTFOLIO
+                </NavLink>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/contact"
+                >
+                    CONTACT
+                </NavLink>
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/resume"
+                >
+                    RESUME
+                </NavLink>
+            </div>
+        </div>
     );
 }
 
